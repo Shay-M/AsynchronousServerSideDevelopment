c8408f04efe348f30dd62db4c49c6b98b38e5712
“ShayM” <shay1mualem@gmail.com>
2021-07-19 18:47:37 +0300

    fix ex2

--- class_2/Student.js
+++ class_2/Student.js
@@ -1,12 +1,25 @@
 class Student {
-    constructor(id, name, average) {
-        if (Number.isNaN(id))
+    constructor(id, name, average, EXTRA = 0) {
+        // if (!Number.isNaN(id))
+        if (parseInt(id.toString()))
             this.id = id;
         else
             throw new ExceptionOfStudent(`id: ' ${id} '  is not a Number! it is a ${typeof (id)}`);
 
-        this.name = name;
-        this.average = average;
+        if (typeof (name) == 'string')
+            this.name = name;
+        else
+            throw new ExceptionOfStudent(`id: ' ${id} '  is not a Number! it is a ${typeof (id)}`);
+
+        if (parseInt(average.toString()))
+            this.average = average;
+        else
+            throw new ExceptionOfStudent(`average: ' ${id} '  is not a Number! it is a ${typeof (id)}`);
+
+        if (EXTRA != 0) {
+            throw new ExceptionOfStudent(`Illegal string format for student. Should be: name,id,average. The surplus: ${EXTRA}`);
+        }
+
     }
 
 
@@ -16,20 +29,15 @@ class Student {
 
     //The from() method returns an Array object from any object with a length property or any iterable object.
     static from(StringOfStudent) {
-
-        let ConvertToStudent = new Student(...StringOfStudent.split(","));
-
-        return ConvertToStudent
-
+        return new Student(...StringOfStudent.split(","));
     }
 
     static fromArrayOfStringStudent(ArrayOfStringStudent) {
 
         for (let i = 0; i < ArrayOfStringStudent.length; i++) {
             ArrayOfStringStudent[i] = ArrayOfStringStudent[i].toString();
-            // Student.validateStudentDataString(ArrayOfStringStudent[i]);
         }
-
+        //Array map() :Return a new array with the square root of all the original values:
         return ArrayOfStringStudent.map(student => Student.from(student));
     }
 
