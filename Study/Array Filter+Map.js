d1f8a983ee9e3cc7252d40943a236e528497fbde
“ShayM” <shay1mualem@gmail.com>
2021-08-12 12:18:11 +0300

    add map

--- Study/Array Filter+Map.js
+++ Study/Array Filter+Map.js
@@ -9,9 +9,25 @@ const filtered = numbers.filter(value => value > 0); //! and can convert to v
 
  */
 
-//* good!
-
+//* good filter
 const filtered = numbers.filter(n => n > 0);
-
-
 console.log(filtered);
+
+///////////////////////////////////////////////
+
+const items = filtered.map(n => '<li>' + n + '</li>');
+
+const html = '<ul>' + items.join('') + '</ul>';
+
+console.log(html);
+
+///////////////////////////////////////////////
+
+
+const itemsObj = numbers
+    .filter(n => n > 0) //* we can not use const filtered
+    .map(n => ({ value: n }))//!  obj so need ()
+    .filter(obj => obj.value > 1);
+
+
+console.log(itemsObj);
