55ae7a5415585d9e9483ab64fb84c40471897284
“ShayM” <shay1mualem@gmail.com>
2021-08-12 13:06:25 +0300

    add Reduce

--- Study/Array Filter+Map.js
+++ Study/Array Filter+Map.js
@@ -1,4 +1,7 @@
 const numbers = [1, -1, 2, 3];
+/* -------------------------------------------------------------------------- */
+/*                                   filter                                   */
+/* -------------------------------------------------------------------------- */
 
 /* const filtered = numbers.filter(function (value) { //! we can convert to => lambda
     return value > 0;//! can remove return bcz we only one val return 
@@ -9,17 +12,18 @@ const filtered = numbers.filter(value => value > 0); //! and can convert to v
 
  */
 
-//* good filter
 const filtered = numbers.filter(n => n > 0);
-console.log(filtered);
+console.log("filtered: " + filtered);
 
-///////////////////////////////////////////////
+/* -------------------------------------------------------------------------- */
+/*                                     map                                    */
+/* -------------------------------------------------------------------------- */
 
 const items = filtered.map(n => '<li>' + n + '</li>');
 
 const html = '<ul>' + items.join('') + '</ul>';
 
-console.log(html);
+console.log("html: " + html);
 
 ///////////////////////////////////////////////
 
@@ -29,5 +33,25 @@ const itemsObj = numbers
     .map(n => ({ value: n }))//!  obj so need ()
     .filter(obj => obj.value > 1);
 
+console.log("itemsObj: " + itemsObj);
 
-console.log(itemsObj);
+/* -------------------------------------------------------------------------- */
+/*                                   Reduce                                   */
+/* -------------------------------------------------------------------------- */
+
+let sum = 0;
+for (let n in numbers)
+    sum += n;
+
+console.log("sum: " + sum); //? why get sum: 00123
+
+//*--------> Better why -> using Reduce
+
+const sumReduce = numbers.reduce((accumulator, currentValue) => { // accumulator is like sum
+    return accumulator + currentValue;
+}, 0); //accumulator = 0 we if not he will be 1 
+
+console.log("sumReduce: " + sumReduce);
+
+
+//* The best why :  const sumReduce = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
