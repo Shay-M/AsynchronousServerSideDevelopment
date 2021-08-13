a43714ed3c20b1f87839df8b557f014b134b0750
“ShayM” <shay1mualem@gmail.com>
2021-08-13 17:17:34 +0300

    update

--- class_3/BmiCalculation.js
+++ class_3/BmiCalculation.js
@@ -1,1 +1,19 @@
-console.log('work!');
+var http = require("http");
+http
+    .createServer(function (req, res) {
+        res.writeHead(200, { ContentTypes: "text/plain" });
+        res.end("Hello World!\n");
+    })
+    .listen(1300);
+
+
+
+var http = require('http');
+var url = require('url');
+http.createServer(function (req, res) {
+    //var area = req.query.width * req.query.height;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    var queryObject = url.parse(req.url, true).query;
+    res.end('area is ' +
+        (queryObject.width * queryObject.height));
+}).listen(process.env.PORT);
