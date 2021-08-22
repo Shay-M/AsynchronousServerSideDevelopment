24f9e7200015eb7d87ada9e3fb07071f37cca5eb
“ShayM” <shay1mualem@gmail.com>
2021-08-22 23:25:14 +0300

    update

--- node - The Complete Guide/routing-requests/app.js
+++ node - The Complete Guide/routing-requests/app.js
@@ -1,50 +1,15 @@
-// https://www.youtube.com/watch?v=7DlZHZF7P3U
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-    if (url === '/') { // When the address only \ created a page
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end(); // stop here and do not continue גד
-    }
-    /* ---------------------------------- POST ---------------------------------- */
+// const server = http.createServer(routes);
+const server = http.createServer(routes.handler);  //*  this version in cases having multi exports
 
-    if (url === '/message' && method === 'POST') {
-        const body = [];
+console.log(routes.someText);
 
-        req.on('data', // [get the req data using 'on'] add a data listener to data event ,every time new chunk is ready to read
-            (chunk) => { // the function to execute when data came
-                console.log(chunk); // <Buffer 6d 65 73 73 61 67 65 3d 66 64 66> we can't work with that
-                body.push(chunk);
-            });
-        return req.on('end', () => {// add a end listener
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const message = parseBody.split('=')[1]; // message = same_text >> 'key' = string
-
-            //! fs.writeFileSync('message.txt', message); // creat a text if use 'writeFileSync' it will block running
-            fs.writeFile('message.txt', message, (err) => { // finish creat file
-                res.statusCode = 302; // like use res.writeHead(302, {'Location', '/'});
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<head><title></title>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
+
+
+
