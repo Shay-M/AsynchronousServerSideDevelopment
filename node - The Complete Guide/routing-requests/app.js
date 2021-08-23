c1b662a03ce32ee762357740f669fc14bfec119d
“ShayM” <shay1mualem@gmail.com>
2021-08-23 10:52:41 +0300

    update

--- node - The Complete Guide/routing-requests/app.js
+++ node - The Complete Guide/routing-requests/app.js
@@ -1,5 +1,7 @@
 // npm init to create json files
 //npm install nodemon --save-dev// https://www.npmjs.com/package/nodemon || development dependency:instal only for development
+// https://dhtmlx.com/blog/top-10-npm-packages-time-guest-post/
+// https://leanylabs.com/blog/npm-packages-for-nodejs/
 /* ------------------------------------ / ----------------------------------- */
 
 const http = require('http');
