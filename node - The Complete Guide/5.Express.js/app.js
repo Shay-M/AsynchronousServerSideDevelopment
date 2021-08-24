d0898c0b1e3b4a9e74ad0c410648d1636c3432c0
“ShayM” <shay1mualem@gmail.com>
2021-08-24 13:12:35 +0300

    update

--- node - The Complete Guide/5.Express.js/app.js
+++ node - The Complete Guide/5.Express.js/app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+//const bodyParser = require('body-parser'); //npm install --save body-parser //! 'bodyParser' is deprecated.
 
 const app = express();
 
-app.use('/', (req, res, next) => {  // will be called every event request|| next is a function
-    console.log('hii from the middleware i always run');
-    next(); // allows the request to continue to the next middleware in line
+// app.use(bodyParser.urlencoded({ extended: false })); //! 'bodyParser' is deprecated.
+
+app.use(express.urlencoded({ extended: false })); //Parse URL-encoded bodies
+// app.use(express.json()); //?Used to parse JSON bodies
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<from></from>')
 });
 
-app.use('/add-item', (req, res, next) => {
-    console.log('hii again! from the middleware add-item');
-    res.send('<h1>hi from add-item</h1>'); //'send' allowed us send a response html or  || the default is html
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
+
 
 app.use('/', (req, res, next) => {
-    console.log('hii again! from the middleware');
-    res.send('<h1>hi</h1>'); //'send' allowed us send a response html or  || the default is html
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
