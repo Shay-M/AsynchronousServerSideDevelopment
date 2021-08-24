8b0dc21f629d4fcde4b54e12599383dc1c24ed55
“ShayM” <shay1mualem@gmail.com>
2021-08-24 12:18:29 +0300

    remove by: npm uninstall nodemon --save-dev

--- node - The Complete Guide/5.Express.js/app.js
+++ node - The Complete Guide/5.Express.js/app.js
@@ -1,17 +1,21 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {  // will be called every event request|| next is a function
-    console.log('hii from the middleware');
+
+app.use('/', (req, res, next) => {  // will be called every event request|| next is a function
+    console.log('hii from the middleware i always run');
     next(); // allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {  // will be called every event request|| next is a function
-    console.log('hii again! from the middleware');
+app.use('/add-item', (req, res, next) => {
+    console.log('hii again! from the middleware add-item');
+    res.send('<h1>hi from add-item</h1>'); //'send' allowed us send a response html or  || the default is html
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('hii again! from the middleware');
+    res.send('<h1>hi</h1>'); //'send' allowed us send a response html or  || the default is html
+});
 
-server.listen(3000);
+app.listen(3000);
+
