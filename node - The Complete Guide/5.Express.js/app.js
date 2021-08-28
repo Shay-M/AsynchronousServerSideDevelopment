0d37cbead31d9ff9e0bc4a1143813b78298ebfe7
“ShayM” <shay1mualem@gmail.com>
2021-08-28 12:06:57 +0300

    serving static files

--- node - The Complete Guide/5.Express.js/app.js
+++ node - The Complete Guide/5.Express.js/app.js
@@ -13,6 +13,8 @@ const shopRouts = require('./routes/shop');
 app.use(express.urlencoded({ extended: false })); //Parse URL-encoded bodies
 //// app.use(express.json()); //?Used to parse JSON bodies
 
+app.use(express.static(path.join(__dirname, 'public'))); // for serving static files(css,image ...), need make 'public' folder public
+
 app.use('/admin', adminRouts);
 
 app.use(shopRouts);
