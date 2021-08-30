abe08926606f01c61b409b1ff355eb2b32ceb739
“ShayM” <shay1mualem@gmail.com>
2021-08-30 10:17:50 +0300

    update

--- node - The Complete Guide/7. The Model View Controller (MVC)/models/product.js
+++ node - The Complete Guide/7. The Model View Controller (MVC)/models/product.js
@@ -1,17 +1,34 @@
-const products = []; // Array of products
+////const products = []; // Array of products//using files to save data
+const fs = require('fs');
+const rootDir = require('../util/path');
+const path = require('path');
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-
+    constructor(t) {
+        this.title = t;
     }
 
     save() {
-        products.push(this);
+        //products.push(this);
+        const pathOfJsonProduct = path.join(
+            path.dirname(process.main.filename),
+            'data', //folder
+            'products.json' // creat a products file in rootDir / data folder path 
+        );
+        fs.readFile(pathOfJsonProduct, (err, fileContent) => {
+            let products = [];
+            if (!err) {//if there a file exist
+                products = JSON.parse(fileContent);//'JSON' helper obj: Converts a JavaScript Object Notation (JSON) string into an object.
+            }
+            products.push(this);//this is for the class bcz =>
+            fs.writeFile(pathOfJsonProduct, JSON.stringify(products), (err) => {
+                // console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
+        return ['products'];
     }
 
 }
--- node - The Complete Guide/7. The Model View Controller (MVC)/util/path.js
+++ node - The Complete Guide/7. The Model View Controller (MVC)/util/path.js
@@ -1,3 +1,4 @@
 const path = require('path');
 
-module.exports = path.dirname(require.main.filename); //! mainModule is deprecated Since v14.0.0 - Use `main` instead.
+//module.exports = path.dirname(require.main.filename);
+//module.exports = path.dirname(process.main.filename); //! mainModule is deprecated Since v14.0.0 - Use `main` instead.
