cb95f45cb5532acb7f1a6d5f6126595cc1c82159
“ShayM” <shay1mualem@gmail.com>
2022-08-20 11:52:16 +0300

    update

--- Study_JS/LeetCode 75/Level 1/102. Binary Tree Level Order Traversal.js
+++ Study_JS/LeetCode 75/Level 1/102. Binary Tree Level Order Traversal.js
@@ -12,6 +12,32 @@ function TreeNode(val, left, right) {
   this.right = right === undefined ? null : right;
 }
 
+/* function levelOrder3(root) {
+  const ans = new Array();
+
+  ans.push([root.val]);
+
+  ans.push(printOrder(root));
+
+  while (root.left || root.right) {
+    ans.push(printOrder(root.left));
+    ans.push(printOrder(root.right));
+
+    // console.log(root);
+    root = root.right;
+  }
+
+  return ans;
+}
+
+function printOrder(node) {
+  const ans2 = new Array();
+
+  if (node.left !== null) ans2.push(node.left.val);
+  if (node.right !== null) ans2.push(node.right.val);
+  return ans2;
+} */
+
 /**
  * @param {TreeNode} root
  * @return {number[][]}
@@ -19,28 +45,50 @@ function TreeNode(val, left, right) {
 var levelOrder = function (root) {
   const ans = new Array();
 
-  //   ans.push([root.val]);
-
-  //   ans.push(printOrder(root));
-
-  ans.push(printOrder(root));
-
-  /* while (root.left || root.right) {
-    ans.push(printOrder(root));
-    // ans.push(printOrder(root.left));
-    // ans.push(printOrder(root.right));
-  } */
+  function fillOrder(root, count = 0) {
+    if (root != null) {
+      if (ans[count] === undefined) {
+        ans[count] = [];
+      }
+      ans[count].push(root.val);
+      fillOrder(root.left, count + 1);
+      fillOrder(root.right, count + 1);
+    }
+  }
+  fillOrder(root);
 
   return ans;
 };
-function printOrder(node) {
-  const ans2 = new Array();
 
-  if (node.left !== null) ans2.push(node.left.val);
-  if (node.right !== null) ans2.push(node.right.val);
-  return ans2;
+//-----------------------------------
+
+function levelOrder2(root) {
+  if (!root) return [];
+
+  const capturedNodes = [];
+  const nodesToTraverse = [root];
+
+  while (nodesToTraverse.length) {
+    //1,2,2
+    const currLevelNodes = nodesToTraverse.length;
+    const level = [];
+
+    for (let i = 0; i < currLevelNodes; i++) {
+      const node = nodesToTraverse.shift();
+      level.push(node.val);
+
+      if (node.left) nodesToTraverse.push(node.left);
+
+      if (node.right) nodesToTraverse.push(node.right);
+    }
+
+    capturedNodes.push(level);
+  }
+
+  return capturedNodes;
 }
 
+// =================================================================
 ListNode7 = new TreeNode(7, null, null);
 ListNode15 = new TreeNode(15, null, null);
 ListNode20 = new TreeNode(20, ListNode15, ListNode7);
@@ -48,3 +96,5 @@ ListNode9 = new TreeNode(9, null, null);
 ListNode3 = new TreeNode(3, ListNode9, ListNode20);
 
 console.log(levelOrder(ListNode3));
+console.log(levelOrder2(ListNode3));
+// console.log(levelOrder3(ListNode3));
